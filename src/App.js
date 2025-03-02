7be2e73813a596fc4ac5bd82293f7cdc0e7ee745
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-03-02 22:15:25 +0530

    modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ const initialItems = [
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
-    <div>
+    <div className="app glass">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,15 +24,16 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now };
-
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -54,11 +60,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
